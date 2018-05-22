80bf59a90438eec90cdd637deb050b62b251d3b3
Vittoria Ferri <Vit@MacBook-Pro-di-Vittoria.local>
2018-05-22 16:39:25 +0200

    Aggiunto codice base kruskal

---
+++ IGraph/src/sample/pair3.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class pair3 {
+
+	public int w, a, b;
+	
+	public pair3(int w, int a, int b) {
+		this.w = w; //weight
+		this.a = a; //nodo a
+		this.b = b; //nodo b
+	}
+	
+	public String toString() {
+		return "" + this.w;
+	}
+}
